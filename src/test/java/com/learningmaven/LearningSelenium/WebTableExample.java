f2df78a76f6749688759111de006cb3d2f3486c8
SurabhiSadanand <surabhisadanand92@gmail.com>
2022-08-18 22:32:14 -0400

    Webtable

--- src/test/java/com/learningmaven/LearningSelenium/WebTableExample.java
+++ src/test/java/com/learningmaven/LearningSelenium/WebTableExample.java
@@ -1,6 +1,5 @@
 package com.learningmaven.LearningSelenium;
 
-
 import java.util.List;
 
 import org.openqa.selenium.By;
@@ -20,15 +19,14 @@ public class WebTableExample {
 	@BeforeMethod
 	public void setUp() {
 		// setting up the chrome driver path
-		System.setProperty("webdriver.chrome.driver", "C:\\\\Drivers\\\\Chrome Driver\\\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", "C:\\Drivers\\Chrome Driver\\chromedriver.exe");
 
 		// Creating Ref. variable and intialising with Chrome driver
 		webDriver = new ChromeDriver();
 
 		wait = new WebDriverWait(webDriver, 10);
 
-		// Get the URL of the page0
-		webDriver.get("https://www.nyse.com/index");
+		webDriver.get("https://chercher.tech/practice/table");
 
 		// Maximise the browser
 		webDriver.manage().window().maximize();
@@ -38,19 +36,19 @@ public class WebTableExample {
 	@Test
 	public void webTableTest() {
 
-		List<WebElement> noOfRows = webDriver.findElements(By.xpath("//table//tbody//tr"));
-		List<WebElement> noOfColumns = webDriver.findElements(By.xpath("//table//thead//tr//th"));
+		List<WebElement> noOfRows = webDriver.findElements(By.xpath("//table[@id='webtable']//tbody//tr"));
+		List<WebElement> noOfColumns = webDriver.findElements(By.xpath("//table[@id='webtable']//tbody//tr//th"));
 		System.out.println("The number of rows: " + noOfRows.size());
 		System.out.println("The number of columns: " + noOfColumns.size());
 
-		String xpathBefore = "//table//tbody//tr[";
+		String xpathBefore = "//table[@id='webtable']//tbody//tr[";
 		String xpathAfter = "]//td[";
 
-		for (int i = 1; i < noOfRows.size(); i++) {
+		for (int i = 2; i < noOfRows.size(); i++) {
 			for (int j = 1; j < noOfColumns.size(); j++) {
-				WebElement element = webDriver.findElement(By.xpath("//table//tbody//tr[" + i + "]//td[" + j + "]"));
-				if (element.getText().equals("NYSE U.S. 100 INDEX")) {
-					System.out.println("Found the entry");
+				WebElement element = webDriver.findElement(By.xpath(xpathBefore + i + xpathAfter + j + "]"));
+				if (element.getText().equals("Google")) {
+					System.out.println("Found the entry Google in the table");
 				}
 
 			}
